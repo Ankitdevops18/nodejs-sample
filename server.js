ff80400769ae059fa38f0cb8fbfc115dfb14d67c
Ankit Solanki <ankitsolanki@Ankits-MacBook-Pro.local>
2025-04-21 11:36:39 +0530

    Updating files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js!');
+  res.send('Hello World from Node.js! This should be Green Cluster ');
 });
 
 app.get('/health', (req, res) => {
