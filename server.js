0359c68f3c47d05248cbcfc25cce8e0a63255c0d
Ankit Solanki <ankitsolanki@Ankits-MacBook-Pro.local>
2025-04-23 07:28:31 +0530

    Updating to Blue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js!');
+  res.send('Hello World from Node.js! Blue');
 });
 
 app.get('/health', (req, res) => {
